9d87efb24c506f6c7df247a9c6ad6a8104e48159
sog-emilly <emilly.soares0101@hotmail.com>
2021-12-08 15:07:22 -0300

    lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,43 @@
+// 1- O código está somando o valor de i, logo a soma final é 10.
+
+
+// 2 - A) Irá retornar todos os números maiores que 18;
+/*        19
+          21
+          23
+          25
+          27
+          30
+*/
+
+/* 2 - B) Sim, é suficiente. Poderiamos acessar dessa forma: 
+console.log(numero[]) e o índice que gostaria de acessar.
+*/
+
+/* 3)
+
+*
+**
+***
+****
+
+Iria adicinar uma linha até chegar à 4 linhas, e adicionaria 1 arterisco em cada linha.
+*/
+
+
+// ESCRITAS DE CÓDIGO
+
+
+//1 - A)
+let numeroAnimaisEstimacao = Number(prompt("Quantos animais de estimação você tem?"))
+    if (numeroAnimaisEstimacao === 0) {
+        console.log("Que pena! Você pode adotar um pet!")
+    } else for (let i = 0; i < numeroAnimaisEstimacao; i++) {
+        const nomesPets = prompt("Digite o nome do seu pet")
+        listaNomes = [nomesPets]
+
+    } 
+   
+
+
+
